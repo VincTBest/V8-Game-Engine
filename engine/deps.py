28b5afbf50e845baf59b7d9bfd69366ff65daf43
VincTBest <vince.nemes2012@gmail.com>
2025-04-07 16:31:58 +0200

    made deps.py

---
+++ engine/deps.py
@@ -0,0 +1,19 @@
+import sys
+import subprocess
+
+Required = ["pygame", "pyopengl"]
+
+
+def install():
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+
+        for module in Required:
+            try:
+                __import__(module)
+            except ImportError:
+                print(f"{module} not found. Installing...")
+                subprocess.check_call([sys.executable, "-m", "pip", "install", module])
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing package(s): {e}")
